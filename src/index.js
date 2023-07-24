07423e35e6b6b65e5df5781891a384514b8750ad
DBHRN <dbhrnf@gmail.com>
2023-07-23 20:38:15 -0600

    cambiando puerto del listen al servidor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import app from './app.js';
 import {connectDB} from './db.js';
 
 connectDB();
-app.listen(5000, () => {
-    console.log(`Server is running on port `, 5000);
+app.listen(3000, () => {
+    console.log(`Server is running on port `, 3000);
 });
