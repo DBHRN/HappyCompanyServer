21d8ecbfcceab7f8194a21d52844055a05193f1a
Susy Esmeralda Díaz Bustillos <108904269+DBHRN@users.noreply.github.com>
2025-02-02 17:56:26 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://dbhrn:Espadaplasma1@atlascluster.goxqfcv.mongodb.net/?retryWrites=true/happycompany');
+        await mongoose.connect('mongodb+srv://dbhrn:Espadaplasma1@atlascluster.goxqfcv.mongodb.net/happycompany');
         console.log('Database connected!');
     } catch (error) {
         console.log(error);
